4311511544227f64a7272d7d48b4d5ee8cbb6588
Bidyut Ranjan Deka <bidyutranjan464@gmail.com>
2018-03-09 18:51:17 +0000

    correct user model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,11 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
-var userSchema = new mongoose.Schema({
-    username: {type:String, unique:true, required:true},
-    password: String,
-    firstName: String,
-    lastName: String,
-    avatar:String,
-    email:{type:String, unique:true, required:true},
-    resetPasswordToken: String,
-    resetPasswordExpires:Date,
-    isAdmin: {type:Boolean, default: false}
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
 });
 
-userSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose)
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
